a828b2e2e79636775e5d863bf03b585526d861e6
jollyjoester <jollyjoester@gmail.com>
2022-02-19 22:49:42 +0900

    Update

---
+++ algorithms/algo-method/Swift/2-8.swift
@@ -0,0 +1,13 @@
+// https://algo-method.com/tasks/26
+
+func main() {
+    let s = readLine()!
+    let t = readLine()!
+    let u = readLine()!
+    
+    let output = u + t + s
+    
+    print(output)
+}
+
+main()
